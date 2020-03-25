143bd54f20b1ddfb6c5ecd052f1fcb56076c5923
AdrianIzad <54908311+AdrianIzad@users.noreply.github.com>
2020-03-25 09:10:45 +0000

    Add files via upload

---
+++ todos.js
@@ -0,0 +1,26 @@
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(1000,function (){
+    $(this).remove();
+    });
+  event.stopPropagation(); /* = this stops any other effects to run */
+});
+
+$("input[type='text']").keypress(function (event){
+  if(event.which === 13){
+    //grabbing new todo text from input
+   var todoText = $(this).val();
+   // gives us an empty input
+   $(this).val("");
+   // create new li and add to ul
+   $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+  }
+});
+
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
